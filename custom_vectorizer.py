5eeddc644ba7b370d4eb27102ad30ddae04462b0
Sahussawud Khunraksa <sahussawudkhunraksa@Sahussawuds-MacBook-Pro.local>
2022-04-12 03:50:22 +0700

    add helper function and customvectorizer

---
+++ custom_vectorizer.py
@@ -0,0 +1,248 @@
+'''custom_tokenizer.py
+- preprocessing data, preserve some word, cut some stop word, clean unrrelated symbol, and preserve a ' - '. it's a sepperate token between message
+- tokenize word with attacut
+- have to avoid counting token with ' - ' '''
+
+# defines a custom vectorizer class.
+from pythainlp.corpus import thai_stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+
+# include file for custom tf-idf
+import scipy.sparse as sp
+from sklearn.utils.validation import check_is_fitted, check_array, FLOAT_DTYPES, check_scalar
+from sklearn.utils import _IS_32BIT
+from sklearn.utils.fixes import _astype_copy_false
+
+#import for reduce()
+import functools
+
+
+class CustomVectorizer(CountVectorizer): 
+    
+    # overwrite the build_analyzer method, and word_ngrams count
+    stopwords = list(thai_stopwords())
+
+    def _word_ngrams(self, tokens, stop_words=None):
+            """Turn tokens into a sequence of n-grams after stop words filtering"""
+            # handle stop words
+            if stop_words is not None:
+                tokens = [w for w in tokens if w not in stop_words]
+            # handle token n-grams
+            min_n, max_n = self.ngram_range
+            if max_n != 1:
+                original_tokens = tokens
+                if min_n == 1:
+                    # no need to do any slicing for unigrams
+                    # just iterate through the original tokens
+                    tokens = list(original_tokens)
+                    min_n += 1
+                else:
+                    tokens = []
+
+                n_original_tokens = len(original_tokens)
+
+                # bind method outside of loop to reduce overhead
+                tokens_append = tokens.append
+                space_join = " ".join
+
+                for n in range(min_n, min(max_n + 1, n_original_tokens + 1)):
+                    for i in range(n_original_tokens - n + 1):
+                        tokens_append(space_join(original_tokens[i : i + n]))
+                tokens = [w for w in tokens if '-' not in w]
+                # print('tokens in loop ',min_n, tokens)
+            return tokens
+
+    # create a custom analyzer for the vectorizer
+    def build_analyzer(self):
+
+        # load stop words using CountVectorizer's built in method
+        # stop_words = self.get_stop_words()
+        
+        # create the analyzer that will be returned by this method
+        def analyser(doc):
+            
+            # apply the preprocessing and tokenzation steps
+            preprocressing_n_tokenizer = customize_text_tokenizer(doc)
+            # remove token that try to count cross message by containing '-' in their token
+            # print('preprocressing_n_tokenizer ', preprocressing_n_tokenizer)
+            remove_separate_token = [word  for word in preprocressing_n_tokenizer if '-' not in word]
+            # print('remove_separate_token ', remove_separate_token)
+            # use CountVectorizer's _word_ngrams built in method
+            # to remove stop words and extract n-grams
+            return(self._word_ngrams(remove_separate_token, self.stopwords))
+        return(analyser)
+
+
+def _document_frequency(X):
+    """Count the number of non-zero values for each feature in sparse X."""
+    if sp.isspmatrix_csr(X):
+        return np.bincount(X.indices, minlength=X.shape[1])
+    else:
+        return np.diff(X.indptr)
+
+class CustomTfidfTranformer(TfidfTransformer): 
+
+  def fit(self, X, y= None):
+    """Learn the idf vector (global term weights).
+        Parameters
+        ----------
+        X : sparse matrix of shape n_samples, n_features)
+            A matrix of term/token counts.
+        y : None
+            This parameter is not needed to compute tf-idf.
+        Returns
+        -------
+        self : object
+            Fitted transformer.
+        """
+    # large sparse data is not supported for 32bit platforms because
+    # _document_frequency uses np.bincount which works on arrays of
+    # dtype NPY_INTP which is int32 for 32bit platforms. See #20923
+    X = self._validate_data(X, accept_sparse=("csr", "csc"), accept_large_sparse=not _IS_32BIT)
+    if not sp.issparse(X):
+      X = sp.csr_matrix(X)
+    dtype = X.dtype if X.dtype in FLOAT_DTYPES else np.float64
+    if self.use_idf:
+      n_samples, n_features = X.shape
+      df = _document_frequency(X)
+      df = df.astype(dtype, ** _astype_copy_false(df))
+      # perform idf smoothing if required
+      df += int(self.smooth_idf)
+      n_samples += int(self.smooth_idf)
+      # log+1 instead of log makes sure terms with zero idf don't get
+      # suppressed entirely.
+      idf = np.log(n_samples / df)
+      self._idf_diag = sp.diags(idf,offsets=0,shape=(n_features, n_features),format="csr",dtype=dtype)
+    return self
+
+
+class CustomTfidfVectorizer(TfidfVectorizer): 
+    def __init__(
+        self,
+        *,
+        input="content",
+        encoding="utf-8",
+        decode_error="strict",
+        strip_accents=None,
+        lowercase=True,
+        preprocessor=None,
+        tokenizer=None,
+        analyzer="word",
+        stop_words=None,
+        token_pattern=r"(?u)\b\w\w+\b",
+        ngram_range=(1, 1),
+        max_df=1.0,
+        min_df=1,
+        max_features=None,
+        vocabulary=None,
+        binary=False,
+        dtype=np.float64,
+        norm="l2",
+        use_idf=True,
+        smooth_idf=True,
+        sublinear_tf=False,
+    ):
+
+        super().__init__(
+            input=input,
+            encoding=encoding,
+            decode_error=decode_error,
+            strip_accents=strip_accents,
+            lowercase=lowercase,
+            preprocessor=preprocessor,
+            tokenizer=tokenizer,
+            analyzer=analyzer,
+            stop_words=stop_words,
+            token_pattern=token_pattern,
+            ngram_range=ngram_range,
+            max_df=max_df,
+            min_df=min_df,
+            max_features=max_features,
+            vocabulary=vocabulary,
+            binary=binary,
+            dtype=dtype,
+        )
+
+        self._tfidf = CustomTfidfTranformer(
+            norm=norm, use_idf=use_idf, smooth_idf=smooth_idf, sublinear_tf=sublinear_tf
+        )
+    # overwrite the build_analyzer method, and word_ngrams count
+    stopwords = list(thai_stopwords())
+    def _word_ngrams(self, tokens, stop_words=None):
+            """Turn tokens into a sequence of n-grams after stop words filtering"""
+            # handle stop words
+            if stop_words is not None:
+                tokens = [w for w in tokens if w not in stop_words]
+            # handle token n-grams
+            min_n, max_n = self.ngram_range
+            if max_n != 1:
+                original_tokens = tokens
+                if min_n == 1:
+                    # no need to do any slicing for unigrams
+                    # just iterate through the original tokens
+                    tokens = list(original_tokens)
+                    min_n += 1
+                else:
+                    tokens = []
+
+                n_original_tokens = len(original_tokens)
+
+                # bind method outside of loop to reduce overhead
+                tokens_append = tokens.append
+                space_join = " ".join
+
+            #     for n in range(min_n, min(max_n + 1, n_original_tokens + 1)):
+            #         for i in range(n_original_tokens - n + 1):
+            #             if '-' not in space_join(original_tokens[i : i + n]):
+            #                 tokens_append(space_join(original_tokens[i : i + n]))
+            # tokens = list(filter(lambda a: a != '-', tokens))
+                for n in range(min_n, min(max_n + 1, n_original_tokens + 1)):
+                        for i in range(n_original_tokens - n + 1):
+                            # print(original_tokens[i : i + n])
+                            tokens_append(space_join(original_tokens[i : i + n]))
+                tokens = [w for w in tokens if '-' not in w]
+            return tokens
+
+    # create a custom analyzer for the vectorizer
+    def build_analyzer(self):
+
+        # load stop words using CountVectorizer's built in method
+        # stop_words = self.get_stop_words()
+        
+        # create the analyzer that will be returned by this method
+        def analyser(doc):
+            
+            # apply the preprocessing and tokenzation steps
+            preprocressing_n_tokenizer = customize_text_tokenizer(doc)
+            # remove token that try to count cross message by containing '-' in their token
+            # print('preprocressing_n_tokenizer ', preprocressing_n_tokenizer)
+            # remove_separate_token = [word  for word in preprocressing_n_tokenizer if '-' not in word]
+            # print('preprocressing_n_tokenizer ', preprocressing_n_tokenizer)
+            # use CountVectorizer's _word_ngrams built in method
+            # to remove stop words and extract n-grams
+            return(self._word_ngrams(preprocressing_n_tokenizer, self.stopwords))
+        return(analyser)
+
+class CustomTfidForTokenizedfVectorizer(CustomTfidfVectorizer):
+    def build_analyzer(self):
+
+          # load stop words using CountVectorizer's built in method
+          # stop_words = self.get_stop_words()
+          
+          # create the analyzer that will be returned by this method
+          def analyser(doc):
+              
+              # apply the preprocessing and tokenzation steps
+              preprocressing_n_tokenizer = doc.split('-')
+              preprocressing_n_tokenizer = [i.split(',') for i in preprocressing_n_tokenizer]
+              result = functools.reduce(lambda a,b:a+['-']+b,preprocressing_n_tokenizer)
+
+              # remove token that try to count cross message by containing '-' in their token
+              # print('preprocressing_n_tokenizer ', preprocressing_n_tokenizer)
+              # remove_separate_token = [word  for word in preprocressing_n_tokenizer if '-' not in word]
+              # print('preprocressing_n_tokenizer ', preprocressing_n_tokenizer)
+              # use CountVectorizer's _word_ngrams built in method
+              # to remove stop words and extract n-grams
+              return(self._word_ngrams(result, self.stopwords))
+          return(analyser)
---
+++ helper_function.py
@@ -0,0 +1,107 @@
+from pythainlp.corpus.common import thai_stopwords
+import string
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import os
+
+def top_tfidf_feats(row, features, top_n=25):
+    ''' Get top n tfidf values in row and return them with their corresponding feature names.'''
+    topn_ids = np.argsort(row)[::-1][:top_n]
+    top_feats = [(features[i], row[i]) for i in topn_ids]
+    df = pd.DataFrame(top_feats)
+    df.columns = ['feature', 'tfidf']
+    return df
+
+def top_feats_in_doc(Xtr, features, row_id, top_n=25):
+    ''' Top tfidf features in specific document (matrix row) '''
+    row = np.squeeze(Xtr[row_id].toarray())
+    return top_tfidf_feats(row, features, top_n)
+
+def top_mean_feats(Xtr, features, grp_ids=None, min_tfidf=0.1, top_n=25):
+    ''' Return the top n features that on average are most important amongst documents in rows
+        indentified by indices in grp_ids. '''
+    if grp_ids:
+        D = Xtr[grp_ids].toarray()
+    else:
+        D = Xtr.toarray()
+
+    D[D < min_tfidf] = 0
+    tfidf_means = np.mean(D, axis=0)
+    return top_tfidf_feats(tfidf_means, features, top_n)
+
+def top_feats_by_class(Xtr, y, features, min_tfidf=0.1, top_n=10):
+    ''' Return a list of dfs, where each df holds top_n features and their mean tfidf value
+        calculated across documents with the same class label. '''
+    dfs = []
+    labels = np.unique(y)
+    for label in labels:
+        ids = np.where(y==label)
+        feats_df = top_mean_feats(Xtr, features, ids, min_tfidf=min_tfidf, top_n=top_n)
+        feats_df.label = label
+        dfs.append(feats_df)
+        print(label)
+    return dfs
+
+def plot_tfidf_classfeats_h(dfs):
+    ''' Plot the data frames returned by the function plot_tfidf_classfeats(). '''
+    fig = plt.figure(figsize=(12, 9), facecolor="w")
+    x = np.arange(len(dfs[0]))
+    for i, df in enumerate(dfs):
+        ax = fig.add_subplot(1, len(dfs), i+1)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_frame_on(False)
+        ax.get_xaxis().tick_bottom()
+        ax.get_yaxis().tick_left()
+        ax.set_xlabel("Mean Tf-Idf Score", labelpad=16, fontsize=14)
+        ax.set_title("label = " + str(df.label), fontsize=16)
+        ax.ticklabel_format(axis='x', style='sci', scilimits=(-2,2))
+        ax.barh(x, df.tfidf, align='center', color='#3F5D7D')
+        ax.set_yticks(x)
+        ax.set_ylim([-1, x[-1]+1])
+        yticks = ax.set_yticklabels(df.feature)
+        plt.subplots_adjust(bottom=0.09, right=0.97, left=0.15, top=0.95, wspace=0.52)
+    plt.show()
+
+def change_matplotlib_font(font_download_url):
+    FONT_PATH = 'MY_FONT'
+    
+    font_download_cmd = f"wget {font_download_url} -O {FONT_PATH}.zip"
+    unzip_cmd = f"unzip -o {FONT_PATH}.zip -d {FONT_PATH}"
+    os.system(font_download_cmd)
+    os.system(unzip_cmd)
+    
+    font_files = fm.findSystemFonts(fontpaths=FONT_PATH)
+    for font_file in font_files:
+        fm.fontManager.addfont(font_file)
+
+    font_name = fm.FontProperties(fname=font_files[0]).get_name()
+    matplotlib.rc('font', family=font_name)
+    print("font family: ", plt.rcParams['font.family'])
+    # import new mathplotlib that attach thai front
+    import matplotlib
+
+def dummy_fun(doc):
+    return doc
+
+def customize_text_tokenizer(reviewText, engine='attacut', split=False):
+
+    without_url = re.sub(r"http\S+", "", reviewText)
+    # replace 55 with 'ฮ่า' before clean word
+    reviewText = re.sub(r"(555)", ' ฮ่า', without_url)
+    # Thai & English Charecter preserve 
+    pattern = re.compile(r"[^\u0E00-\u0E7Fa-zA-Z']|^'|'$|''|[//t//n]|^#")
+    char_to_remove = re.findall(pattern, reviewText)
+    # preserve sepperate token -
+    char_to_remove = [i for i in char_to_remove if i != '-']
+    # remove ignore charecters
+    list_with_char_removed = [char for char in reviewText if not char in char_to_remove]
+    nopunc_text = ''.join(list_with_char_removed).strip()
+    # tokenize word with attacut
+    word_tokenized = word_tokenize(nopunc_text, engine=engine, keep_whitespace=True) 
+
+    return word_tokenized
+
+font_download_url = "https://fonts.google.com/download?family=Sarabun"
+change_matplotlib_font(font_download_url)
